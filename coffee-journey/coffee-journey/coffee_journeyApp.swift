2e94162af6a06886e39efc47559236e85b21b764
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-10-24 17:00:57 +0200

    initial project setup

---
+++ coffee-journey/coffee-journey/coffee_journeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_journeyApp.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 24.10.25.
+//
+
+import SwiftUI
+
+@main
+struct coffee_journeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
