c33ac80caa89d775c39ba654ee589875cd7b033b
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-10-27 17:12:27 +0100

    move CreateCoffeeRequest struct into its own file

---
+++ coffee-journey/coffee-journey/CreateCoffeeRequest.swift
@@ -0,0 +1,27 @@
+//
+//  CreateCoffeeRequest.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 27.10.25.
+//
+
+import Foundation
+
+
+enum CoffeeCreationError: Error {
+    case emptyName
+    case invalidRating
+}
+
+enum RoastCategory: String, Codable, CaseIterable {
+    case light, medium, dark
+}
+
+struct CreateCoffeeRequest {
+    let name: String
+    let roaster: String
+    let roastCategory: RoastCategory
+    let roastDate: Date
+    let rating: Double
+    let notes: String
+}
--- coffee-journey/coffee-journey/UseCases.swift
+++ coffee-journey/coffee-journey/UseCases.swift
@@ -8,23 +8,7 @@
 import Foundation
 import SwiftUI
 
-enum CoffeeCreationError: Error {
-    case emptyName
-    case invalidRating
-}
 
-enum RoastCategory: String, Codable, CaseIterable {
-    case light, medium, dark
-}
-
-struct CreateCoffeeRequest {
-    let name: String
-    let roaster: String
-    let roastCategory: RoastCategory
-    let roastDate: Date
-    let rating: Double
-    let notes: String
-}
 
 struct UseCases {
     var createCoffee: @MainActor (CreateCoffeeRequest) throws -> Coffee
