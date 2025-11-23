df0098f56edd627696097ca4efe02e71b406dcd5
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-11-23 22:02:44 +0100

    Prepare a RefillModel

---
+++ coffee-journey/coffee-journey/Models/RefillModel.swift
@@ -0,0 +1,15 @@
+//
+//  RefillModel.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 23.11.25.
+//
+
+import Foundation
+
+
+struct Refill {
+    var amount: Double
+    var roastDate: Date
+    var date: Date
+}
