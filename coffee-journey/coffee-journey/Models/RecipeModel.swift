e0ccdf94eaf02199fbaa090ce0417fe6f513774a
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-11-23 17:42:17 +0100

    add beginning of RecipeModel

---
+++ coffee-journey/coffee-journey/Models/RecipeModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeModel.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 23.11.25.
+//
+
+import Foundation
+
+
+struct Recipe {
+    var amountCoffee: Double
+    var amountWater: Double
+}
