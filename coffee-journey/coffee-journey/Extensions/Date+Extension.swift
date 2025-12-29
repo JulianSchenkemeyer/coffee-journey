4a54e1d03d7bfc9c9e81d2384cf48e81022bc7e6
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-12-29 12:01:00 +0100

    add additional convenience vars to Date

--- coffee-journey/coffee-journey/Extensions/Date+Extension.swift
+++ coffee-journey/coffee-journey/Extensions/Date+Extension.swift
@@ -9,4 +9,8 @@ import Foundation
 
 extension Date {
     static var oneYearAgo: Date { Calendar.current.date(byAdding: .year, value: -1, to: Date())! }
+    
+    static var sixMonthsAgo: Date { Calendar.current.date(byAdding: .month, value: -6, to: Date())! }
+    
+    static var threeMonthsAgo: Date { Calendar.current.date(byAdding: .month, value: -3, to: Date())! }
 }
