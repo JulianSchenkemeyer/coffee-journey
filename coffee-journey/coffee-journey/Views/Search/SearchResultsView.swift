b3e48f688faf3dea175adb98bb8a1c3305157147
Julian Schenkemeyer <julian@jschenkemeyer.com>
2026-01-31 15:43:45 +0100

    update SearchView to make use of the SearchableModel and ResultRowRenderer protocol

--- coffee-journey/coffee-journey/Views/Search/SearchResultsView.swift
+++ coffee-journey/coffee-journey/Views/Search/SearchResultsView.swift
@@ -6,15 +6,33 @@
 //
 import Foundation
 import SwiftUI
+import SwiftData
 
 
-struct SearchResultsView: View {
+struct SearchResultsView<T: SearchableModel, R: ResultRowRenderer>: View where R.Model == T {
+    let searchTerm: String
+    let renderer: R
+
+    @Query private var items: [T]
+
+    init(searchTerm: String, renderer: R) {
+        self.searchTerm = searchTerm
+        self.renderer = renderer
+        if let predicate = T.search(for: searchTerm) {
+            _items = Query(filter: predicate)
+        } else {
+            _items = Query()
+        }
+    }
+
     var body: some View {
-        Text("SearchResults")
+        List(items) { item in
+            renderer.row(for: item)
+        }
     }
 }
 
 
 #Preview {
-    SearchResultsView()
+    SearchResultsView(searchTerm: "Test", renderer: CoffeeRowRenderer())
 }
--- coffee-journey/coffee-journey/Views/Search/SearchView.swift
+++ coffee-journey/coffee-journey/Views/Search/SearchView.swift
@@ -9,24 +9,51 @@ import SwiftUI
 
 
 struct SearchView: View {
+    enum SearchType: String, Hashable, CaseIterable, Identifiable {
+        case coffee = "Coffee"
+        case equiment = "Equipment"
+        
+        var id: String { rawValue }
+    }
+    
     @State private var searchTerm: String = ""
+    @State private var selectedType = SearchType.coffee
     
     var body: some View {
         NavigationStack {
-            Group {
+            VStack(spacing: 24) {
+                Picker("Type", selection: $selectedType) {
+                    ForEach(SearchType.allCases, id: \.self) { type in
+                        Text(type.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal, 48)
+                
                 if searchTerm.isEmpty {
                     RecentSearchesView()
                 } else {
-                    SearchResultsView()
+                    switch selectedType {
+                    case .coffee:
+                        SearchResultsView(searchTerm: searchTerm, renderer: CoffeeRowRenderer())
+                    case .equiment:
+                        SearchResultsView(searchTerm: searchTerm, renderer: EquipmentRowRenderer())
+                    }
                 }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
             .navigationTitle("Search")
             .searchable(text: $searchTerm)
         }
     }
+    
+    
 }
 
 
 #Preview {
-    SearchView()
+    PreviewUseCaseEnvironment {
+        SearchView()
+    }
 }
+
