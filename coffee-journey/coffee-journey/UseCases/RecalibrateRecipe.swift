13cda95c0c7f26529d609e7f1d2616b834ae2d3d
Julian Schenkemeyer <julian@jschenkemeyer.com>
2026-02-01 19:02:09 +0100

    add RecalibrateRecipe use case

---
+++ coffee-journey/coffee-journey/UseCases/RecalibrateRecipe.swift
@@ -0,0 +1,37 @@
+//
+//  RecalibrateRecipe.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 01.02.26.
+//
+import Foundation
+
+
+@MainActor struct RecalibrateRecipe {
+    let repository: RecipeRepository
+    
+    @discardableResult
+    func callAsFunction(recipe: Recipe) throws -> Recipe {
+        let avgTemperature = recipe.temperature
+        recipe.maxTemperature = avgTemperature
+        recipe.minTemperature = avgTemperature
+        
+        let avgExtractionTime = recipe.extractionTime
+        recipe.maxExtractionTime = avgExtractionTime
+        recipe.minExtractionTime = avgExtractionTime
+        
+        let avgAmountBeans = recipe.amountBeans
+        recipe.maxAmountBeans = avgAmountBeans
+        recipe.minAmountBeans = avgAmountBeans
+        
+        let avgGrindSize = recipe.grindSize
+        recipe.maxGrindSize = avgGrindSize
+        recipe.minGrindSize = avgGrindSize
+        
+        let avgOutput = recipe.output
+        recipe.maxOutput = avgOutput
+        recipe.minOutput = avgOutput
+        
+        return try repository.update(recipe)
+    }
+}
