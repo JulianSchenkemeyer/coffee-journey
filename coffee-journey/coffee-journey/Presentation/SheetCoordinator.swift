574180232d88cb69c631a549f203056b7ae3cfcf
Julian Schenkemeyer <julian@jschenkemeyer.com>
2026-02-09 20:33:23 +0100

    implement SheetCoordinator

---
+++ coffee-journey/coffee-journey/Presentation/SheetCoordinator.swift
@@ -0,0 +1,49 @@
+//
+//  SheetCoordinator.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 09.02.26.
+//
+import Foundation
+import SwiftUI
+
+
+@Observable class SheetCoordinator {
+    enum ActiveSheet: Identifiable {
+        case brew(Coffee)
+        case refill(Coffee)
+        case addCoffee
+        case editRecipe(Recipe)
+        
+        var id: String {
+            switch self {
+            case .brew: "brew"
+            case .refill: "refill"
+            case .addCoffee: "addCoffee"
+            case .editRecipe: "editRecipe"
+            }
+        }
+    }
+    
+    var activeSheet: ActiveSheet?
+    
+    func present(_ sheet: ActiveSheet) {
+        activeSheet = sheet
+    }
+    
+    func dismiss() {
+        activeSheet = nil
+    }
+}
+
+// Environment key for SheetManager
+private struct SheetCoordinatorKey: EnvironmentKey {
+    static let defaultValue = SheetCoordinator()
+}
+
+extension EnvironmentValues {
+    var sheetCoordinator: SheetCoordinator {
+        get { self[SheetCoordinatorKey.self] }
+        set { self[SheetCoordinatorKey.self] = newValue }
+    }
+}
