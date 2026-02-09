86827a3e270df32375bbb3b42d32b39074b4b94b
Julian Schenkemeyer <julian@jschenkemeyer.com>
2026-02-09 20:34:26 +0100

    create a global place to manage sheets

--- coffee-journey/coffee-journey/ContentView.swift
+++ coffee-journey/coffee-journey/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.useCases) private var useCases
+    @Environment(\.sheetCoordinator) private var sheetCoordinator
         
     var body: some View {
+        @Bindable var sheetCoordinator = sheetCoordinator
+        
         TabView {
             Tab("Coffee", systemImage: "cup.and.heat.waves") {
                 CoffeeShelfView()
@@ -21,6 +24,20 @@ struct ContentView: View {
             
             Tab(role: .search) {
                 SearchView()
+            }
+        }
+        .sheet(item: $sheetCoordinator.activeSheet) { sheet in
+            switch sheet {
+            case .brew(let coffee):
+                BrewDrinkModalView(coffee: coffee)
+                    .presentationDetents([.fraction(0.9)])
+            case .refill(let coffee):
+                RefillBeansModalView(coffee: coffee)
+                    .presentationDetents([.medium])
+            case .addCoffee:
+                AddCoffeeFormView()
+            case .editRecipe(let recipe):
+                Text("Edit Recipe: \(recipe.name)")
             }
         }
     }
