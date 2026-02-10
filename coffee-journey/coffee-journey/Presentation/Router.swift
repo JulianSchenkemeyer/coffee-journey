5bf9745ff519993b8ca8b28ce81c5831b34e546f
Julian Schenkemeyer <julian@jschenkemeyer.com>
2026-02-10 20:01:02 +0100

    implement router

---
+++ coffee-journey/coffee-journey/Presentation/Router.swift
@@ -0,0 +1,52 @@
+//
+//  Router.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 09.02.26.
+//
+
+import SwiftUI
+
+@Observable
+class Router {
+    var path: [Route] = []
+    
+    enum Route: Hashable {
+        case coffeeDetails(Coffee)
+        case equipmentDetails(Equipment)
+    }
+    
+    // Navigation methods
+    func navigate(to route: Route) {
+        path.append(route)
+    }
+    
+    func navigateBack() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+    
+    func navigateToRoot() {
+        path.removeAll()
+    }
+    
+    func pop(count: Int = 1) {
+        guard path.count >= count else {
+            path.removeAll()
+            return
+        }
+        path.removeLast(count)
+    }
+}
+
+// Environment key for Router
+private struct RouterKey: EnvironmentKey {
+    static let defaultValue = Router()
+}
+
+extension EnvironmentValues {
+    var router: Router {
+        get { self[RouterKey.self] }
+        set { self[RouterKey.self] = newValue }
+    }
+}
