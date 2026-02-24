f6f073d01450b1464b7575c7dd8b1e04648ac7d1
Julian Schenkemeyer <julian@jschenkemeyer.com>
2026-02-24 22:36:07 +0100

    add a CoffeeConstants file to collect the „magic numbers“ of Coffee

---
+++ coffee-journey/coffee-journey/Constants/CoffeeConstants.swift
@@ -0,0 +1,20 @@
+//
+//  CoffeeConstants.swift
+//  coffee-journey
+//
+//  Created by Julian Schenkemeyer on 24.02.26.
+//
+
+enum CoffeeConstants {
+    enum Amount {
+        static let range: ClosedRange<Double> = 0.0...2000.0
+        static let step: Double = 5.0
+        static let defaultValue: Double = 250.0
+    }
+
+    enum Rating {
+        static let range: ClosedRange<Double> = 0.0...5.0
+        static let step: Double = 0.5
+        static let defaultValue: Double = 3.0
+    }
+}
