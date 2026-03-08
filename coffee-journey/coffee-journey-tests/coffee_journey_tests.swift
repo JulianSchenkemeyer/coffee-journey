84d215e161ef11a0f5a97148a6b4f0fc95a53410
Julian Schenkemeyer <julian@jschenkemeyer.com>
2026-03-08 16:07:53 +0100

    add testing target

---
+++ coffee-journey/coffee-journey-tests/coffee_journey_tests.swift
@@ -0,0 +1,16 @@
+//
+//  coffee_journey_tests.swift
+//  coffee-journey-tests
+//
+//  Created by Julian Schenkemeyer on 08.03.26.
+//
+
+import Testing
+
+struct coffee_journey_tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
